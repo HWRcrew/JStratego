02a554a8ab5c96e3593672aa18761d965e191ba5
Sebastian Grosse <gro.s1991@googlemail.com>
2012-06-03 19:19:04 +0200

    Klassen der Figuren erstellt. extends Piece

---
+++ src/pieces/Miner.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pieces;
+
+/**
+ *
+ * @author sebastiangrosse
+ */
+public class Miner extends Piece{
+	
+}
