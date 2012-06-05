a9295113143a7e1156c62b65384ca10723dd95ab
Sebastian Grosse <gro.s1991@googlemail.com>
2012-06-05 23:03:25 +0200

    erste Liste gefüllt

--- src/pieces/Flag.java
+++ src/pieces/Flag.java
@@ -4,10 +4,27 @@
  */
 package pieces;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author sebastiangrosse
  */
 public class Flag extends Piece {
+
+	public Flag(String name, String description, ArrayList<String> beatenby, String c, String iurl) {
+		super(name, description, beatenby, c, iurl);
+		beatenby.add("Marshal");
+		beatenby.add("General");
+		beatenby.add("Colonel");
+		beatenby.add("Major");
+		beatenby.add("Captain");
+		beatenby.add("Lieutenant");
+		beatenby.add("Sergeant");
+		beatenby.add("Miner");
+		beatenby.add("Scout");
+		beatenby.add("Spy");
+		beatenby.add("Bomb");
+	}
 	// TODO beatenby-Liste erstellen und füllen.
 }
--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -15,10 +15,18 @@ public abstract class Piece {
 	static private String NAME;
 	static private String DESCRIPTION;
 	static private ArrayList<String> BEATENBY;
+	private String color;
+	static private String imgurl;
+	//TODO entscheiden ob diese Eigenschaften unnütz sind.
+	private boolean alive;
+	private boolean covered;
+	private boolean movable;
 
-	public Piece(String name, String description, ArrayList<String> beatenby) {
+	public Piece(String name, String description, ArrayList<String> beatenby, String c, String iurl) {
 		NAME = name;
 		DESCRIPTION = description;
 		BEATENBY = beatenby;
+		color = c;
+		imgurl = iurl;
 	}
 }
