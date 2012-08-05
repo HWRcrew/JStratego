287cd30bf135cb713b0da9a3381f8bfcf8bf8b06
Sebastian Grosse <gro.s1991@googlemail.com>
2012-08-05 20:39:12 +0200

    Methode um über englischen Figurnamen eine Figur aus der Liste des Spielers zu bekommen

--- src/jstratego/logic/game/Player.java
+++ src/jstratego/logic/game/Player.java
@@ -14,10 +14,87 @@ public class Player {
 	public String name;
 	public Color playerColor;
 	public List<Piece> pieces = new ArrayList<Piece>();
+
 	public Player(String name, Color playerColor) {
 		this.name = name;
 		this.playerColor = playerColor;
 		handoutPieces();
+	}
+
+	public Piece getPiece(String typ) {
+		if (typ.equals("Marshal")) {
+			int index = this.pieces.indexOf(Marshal.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("General")) {
+			int index = this.pieces.indexOf(General.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Colonel")) {
+			int index = this.pieces.indexOf(Colonel.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Major")) {
+			int index = this.pieces.indexOf(Major.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Captain")) {
+			int index = this.pieces.indexOf(Captain.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Lieutenant")) {
+			int index = this.pieces.indexOf(Lieutenant.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Sergeant")) {
+			int index = this.pieces.indexOf(Sergeant.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Miner")) {
+			int index = this.pieces.indexOf(Miner.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Scout")) {
+			int index = this.pieces.indexOf(Scout.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Spy")) {
+			int index = this.pieces.indexOf(Spy.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Bomb")) {
+			int index = this.pieces.indexOf(Bomb.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		if (typ.equals("Flag")) {
+			int index = this.pieces.indexOf(Flag.class);
+			if (index >= 0) {
+				return this.pieces.get(index);
+			}
+		}
+		return null;
 	}
 
 	private void handoutPieces() {
