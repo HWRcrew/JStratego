1d59ab10970eb16f24e5cfa7c51a05b08e593dc7
Sebastian Grosse <gro.s1991@googlemail.com>
2012-08-10 08:24:41 +0200

    added TODO's

--- src/jstratego/JStratego.java
+++ src/jstratego/JStratego.java
@@ -15,4 +15,11 @@ public class JStratego {
 		// TODO code application logic here
             StartScreen.main(args);
 	}
+	//TODO ActionListener (eigene Klasse) für alle Spielphasen merkt sich zwei klicks und agiert entsprechend des letzen klicks
+	//TODO refresher mit Werteübergabe - Element, das erneuert werden soll
+	//TODO abstrakte Klasse für GUI-Elemente die dem refresher übergeben werden kann
+	//TODO Klasse für Spielbrett und Infotable getrennt & komplettes Spielbrett in Game zusammenführen
+	//TODO eigene Klasse für die felder auf dem Spielbrett, werden dann im spielbrett integriert
+	//TODO Merken einer aktion in der move-phase bestätigen und ausführen der Logik erst durch click auf Zug beenden
+	//TODO eigene KLasse für Change-GUI anzeigen des letzten kampfes falls geschehen.
 }
